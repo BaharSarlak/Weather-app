920f0a3c79eb17fd95248a02adc87a50396804ab
BaharSarlak <bsarlak98@gmail.com>
2022-02-05 15:41:57 -0500

    generated a form to query city name and send back weather

--- server.js
+++ server.js
@@ -4,23 +4,30 @@ const appId=require('./appId.js');
 
 const app=express();
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.listen(3000, 'localhost', ()=>console.log('server running on port 3000'));
 
 app.get('/', (req,res)=>{
-    const unitsSystem='metric'
-    const londonUrl=`https://api.openweathermap.org/data/2.5/find?q=London&units=${unitsSystem}&appid=${appId}`;
-    https.get(londonUrl, (response)=>{
+    res.sendFile(__dirname+'/index.html');
+})
+
+app.post('/', (req,res)=>{
+    const query=req.body.cityName
+    const unitsSystem='metric';
+    const cityUrl=`https://api.openweathermap.org/data/2.5/find?q=${query}&units=${unitsSystem}&appid=${appId}`;
+    https.get(cityUrl, (response)=>{
         console.log('response status code: '+ response.statusCode);
-        response.on('data',d=>{
-            const londonData= JSON.parse(d);
-            const londonTemp=londonData.list[0].main.temp
-            const londonDescription=londonData.list[0].weather[0].description
-            const londonIconUrl=`http://openweathermap.org/img/wn/${londonData.list[0].weather[0].icon}@2x.png`
-            console.log(londonIconUrl)
-            console.log('Tempreture: '+londonTemp +' Celcius | weather description: '+ londonDescription)
-            res.write(`<h1>Temperature in London is: ${londonTemp}</h1>`)
-            res.write(`Weather condition is: ${londonDescription}`)
-            res.write(`<img src=${londonIconUrl}></img>`)
+        response.on('data', d=>{
+            const cityData= JSON.parse(d);
+            const cityTemp=cityData.list[0].main.temp;
+            const cityDescription=cityData.list[0].weather[0].description;
+            const cityIconUrl=`http://openweathermap.org/img/wn/${cityData.list[0].weather[0].icon}@2x.png`
+            console.log(`${query}: Tempreture: ${cityTemp} +' Celcius | weather description: ${cityDescription}`);
+            res.write(`<h1>Temperature in ${query} is: ${cityTemp}</h1>`);
+            res.write(`Weather condition is: ${cityDescription}`);
+            res.write(`<img src=${cityIconUrl}></img>`);
             res.send()
         })
     })
